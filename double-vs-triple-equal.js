c8acdcaf86e795b46c6a91c7ddb16e8f1d6405c2
RahatRahman <mahmudur665@gmail.com>
2020-08-08 23:02:35 +0600

    updated

--- double-vs-triple-equal.js
+++ double-vs-triple-equal.js
@@ -1,12 +1,9 @@
-// const first = 2;
-// const second = "2";
-// const first = 1;
-// const second = true;
-// if (first === second) {
-//   console.log("Condition is true");
-// } else {
-//   console.log("Condition is false");
-// }
-
-
-if (‘2’===2){  console.log(“Inside if”);  }  else{  console.log(“Inside else”);  }
+const first = 2;
+const second = "2";
+const first = 1;
+const second = true;
+if (first === second) {
+  console.log("Condition is true");
+} else {
+  console.log("Condition is false");
+}
