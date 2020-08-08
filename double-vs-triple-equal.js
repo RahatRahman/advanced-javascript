4baef61676ec611a44c461cbece45d4dbff82104
RahatRahman <mahmudur665@gmail.com>
2020-08-08 12:48:57 +0600

    double equal vs triple equal usage

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,12 @@
+// const first = 2;
+// const second = "2";
+// const first = 1;
+// const second = true;
+// if (first === second) {
+//   console.log("Condition is true");
+// } else {
+//   console.log("Condition is false");
+// }
+
+
+if (‘2’===2){  console.log(“Inside if”);  }  else{  console.log(“Inside else”);  }
