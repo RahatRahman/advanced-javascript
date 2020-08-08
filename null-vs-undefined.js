d1c21296b15569e289166f28fd8a714c2159fc4f
RahatRahman <mahmudur665@gmail.com>
2020-08-08 12:29:29 +0600

    null vs undefined interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+let name;
+function add(num1, num2) {
+  console.log(num1 + num2);
+  return;
+}
+
+function add(num1, num2) {
+  console.log(num1, num2);
+}
+
+const student = { name: "Omuk", id: 123456 };
+console.log(student.email);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [1, 3, 5];
+console.log(ages[11]);
